9f664d8e1dc302cbce3f6a613b4f519534de37fd
Jean Carlos <avilamix.ac@outlook.com>
2026-05-05 06:50:25 +0000

    feat: add vercel.json + dynamic base path for dual deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/checklist-fornecedores-cobija/',
+  base: process.env.GITHUB_PAGES ? '/checklist-fornecedores-cobija/' : '/',
   server: {
     port: 3000,
     open: true
